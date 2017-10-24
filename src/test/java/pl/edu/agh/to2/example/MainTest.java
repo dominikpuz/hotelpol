96baff7c7245f25a535da90f3d4ac7f81f17ae2e
Kamil Piętak <kpietak@agh.edu.pl>
2017-10-24 21:12:04 +0200

    sample test added

---
+++ src/test/java/pl/edu/agh/to2/example/MainTest.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to2.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+}
