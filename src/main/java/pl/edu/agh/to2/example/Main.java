e45c7c9aa89b9b4c93d9363d9c703f640aa8a987
Kamil Piętak <kpietak@agh.edu.pl>
2017-10-24 21:08:08 +0200

    logger introduced instead of sysout

--- src/main/java/pl/edu/agh/to2/example/Main.java
+++ src/main/java/pl/edu/agh/to2/example/Main.java
@@ -1,9 +1,13 @@
 package pl.edu.agh.to2.example;
 
+import java.util.logging.Logger;
+
 public class Main {
 
+	private static final Logger log = Logger.getLogger(Main.class.toString());
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		log.info("Hello world");
 
 	}
 
